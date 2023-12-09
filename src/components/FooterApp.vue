c93782e858ceb39d8f59e4615eae3b367cb9cdea
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-09 14:07:59 +0100

    feat: FooterBot add social

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,260 @@
+<script>
+import FooterBot from './FooterBot.vue';
+export default {
+  name: 'topFooter',
+  data() {
+    return {
+      linkFooter1: [
+        {
+          title: 'DC Comics',
+          name: 'Characters',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Comics',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Movies',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Tv',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Games',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Collectibles',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Videos',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Fans',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'News',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Shop',
+          link: '#',
+          target: '_self',
+        },
+      ],
+
+      linkFooter2: [
+        {
+          title: 'DC',
+          name: 'Terms Of Use',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Priacy policy (New)',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Ad Choices',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Advertising',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Jobs',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Sunscriptions',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Talent Workshops',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'CPSC Certificates',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Rating',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Shop Help',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Contact us',
+          link: '#',
+          target: '_self',
+        },
+      ],
+
+      linkFooter3: [
+        {
+          title: 'sites',
+          name: 'DC',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'MAD Magazine',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'DC Kids',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'DC Universe',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'DC Power Visa',
+          link: '#',
+          target: '_self',
+        },
+      ],
+
+      linkFooter4: [
+        {
+          title: 'shop',
+          name: ' Shop DC',
+          link: '#',
+          target: '_self',
+        },
+        {
+          name: 'Shop DC Collectibles',
+          link: '#',
+          target: '_self',
+        },
+      ],
+    };
+  },
+  components: {
+    FooterBot,
+  },
+};
+</script>
+<template>
+  <section class="bg-image">
+    <div class="container">
+      <div class="col-1">
+        <ul>
+          <li>
+            <h3>{{ linkFooter1[0].title }}</h3>
+          </li>
+          <li v-for="li in linkFooter1">
+            <a href="{{ li.link }}">{{ li.name }}</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <h3>{{ linkFooter2[0].title }}</h3>
+          </li>
+          <li v-for="li in linkFooter2">
+            <a href="{{ li.link }}">{{ li.name }}</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <h3>{{ linkFooter3[0].title }}</h3>
+          </li>
+          <li v-for="li in linkFooter3">
+            <a href="{{ li.link }}">{{ li.name }}</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <h3>{{ linkFooter4[0].title }}</h3>
+          </li>
+          <li v-for="li in linkFooter4">
+            <a href="{{ li.link }}">{{ li.name }}</a>
+          </li>
+        </ul>
+      </div>
+      <img src="../../public/img/dc-logo-bg.png" alt="" />
+    </div>
+  </section>
+  <FooterBot></FooterBot>
+</template>
+<style scoped lang="scss">
+@use '../style/partials' as *;
+
+.bg-image {
+  background-image: url(../../public/img/footer-bg.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: top, right;
+  overflow: hidden;
+
+  .container {
+    height: 100%;
+    align-items: flex-end;
+    position: relative;
+    .col-1 {
+      width: calc(100% / 3);
+      flex-wrap: wrap;
+      display: flex;
+      padding: 30px 0;
+
+      ul {
+        margin-right: 30px;
+
+        li {
+          color: #959595;
+          line-height: 1.6;
+
+          &:hover {
+            color: white;
+          }
+
+          h3 {
+            color: white;
+            margin-bottom: 10px;
+            text-transform: uppercase;
+          }
+        }
+      }
+    }
+
+    img {
+      position: absolute;
+      right: 0;
+      top: -100px;
+      width: 630px;
+    }
+  }
+}
+</style>
---
+++ src/components/FooterBot.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+  name: 'FooterBottom',
+  data() {
+    return {
+      socials: [
+        {
+          name: 'facebook',
+          link: '#',
+          target: '_self',
+          img: '../../public/img/footer-facebook.png',
+        },
+        {
+          name: 'twitter',
+          link: '#',
+          target: '_self',
+          img: '../../public/img/footer-twitter.png',
+        },
+        {
+          name: 'youtube',
+          link: '#',
+          target: '_self',
+          img: '../../public/img/footer-youtube.png',
+        },
+        {
+          name: 'pinterest',
+          link: '#',
+          target: '_self',
+          img: '../../public/img/footer-pinterest.png',
+        },
+        {
+          name: 'periscope',
+          link: '#',
+          target: '_self',
+          img: '../../public/img/footer-periscope.png',
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <section>
+    <div class="container">
+      <div class="col-1">
+        <button><a href="">Sing-up Now!</a></button>
+      </div>
+      <div class="col-2">
+        <h3>Follow us</h3>
+        <ul>
+          <li v-for="li in socials">
+            <img :src="li.img" alt="" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped lang="scss">
+@use '../style/partials' as *;
+section {
+  background-color: #303030;
+  padding: 30px 0;
+
+  button {
+    padding: 15px 30px;
+    border: 2px solid #0282f9;
+    text-transform: uppercase;
+    color: white;
+    font-weight: bolder;
+  }
+
+  .col-2 {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+
+    h3 {
+      color: #0282f9;
+      text-transform: uppercase;
+    }
+
+    ul {
+      display: flex;
+      gap: 20px;
+    }
+  }
+}
+</style>
